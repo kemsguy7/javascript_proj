e3dc5c0dfb991129724a99e3335bb2b38a7e50fb
Matthew Idungafa <mattidungafa@gmail.com>
2024-04-03 05:40:33 +0100

    done with simple color changer

--- setup/app.js
+++ setup/app.js
@@ -4,5 +4,15 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function() { 
-    console.log(document)
+    // get rendom number between 0 - 3
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+    console.log(randomNumber);
+    
 })
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length); 
+}
+
