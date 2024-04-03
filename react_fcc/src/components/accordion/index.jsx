fb4fc32a3d64c33fbc34d33e5bb76b1a6d88e329
Matthew Idungafa <mattidungafa@gmail.com>
2024-04-03 14:39:44 +0100

    updates

--- react_fcc/src/components/accordion/index.jsx
+++ react_fcc/src/components/accordion/index.jsx
@@ -1,8 +1,39 @@
-//single selection 
+//single selection
 //multiple selection
+import React, { useState } from 'react'
+import data from './data'
 
-export default function Accordion ( ) {
-    const [selected, setSelected] = useState(null);
+export default function Accordion() {
+  const [selected, setSelected] = useState(null)
 
-    return <div> </div> 
+  function handleSingleSelection(getCurrentId) {
+    console.log(getCurrentId)
+    setSelected(getCurrentId)
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div onClick={() => handleSingleSelection()} className="title">
+                <h3> {dataItem.question} </h3>
+                <span> + </span>
+              </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : (
+                <div className="content" style={{ display: 'none' }}>
+                  {dataItem.answer}
+                </div>
+              )}
+            </div>
+          ))
+        ) : (
+          <div> No data found ! </div>
+        )}
+      </div>
+    </div>
+  )
 }
