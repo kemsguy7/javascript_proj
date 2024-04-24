18946c77c64bfafdfceaa8323ec928392a879701
Matthew Idungafa <mattidungafa@gmail.com>
2024-04-24 08:53:58 +0100

    updates on questions

---
+++ questions/app.js
@@ -0,0 +1,11 @@
+//using selectors inside the element
+
+//traversing the DOM
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        console.log(e.currentTarget); 
+    })
+})
