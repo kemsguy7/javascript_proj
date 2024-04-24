ecab5a0999b25648f8c39fcf59f60dd76e39c697
Matthew Idungafa <mattidungafa@gmail.com>
2024-04-24 12:09:35 +0100

    updates on accordion

--- questions/app.js
+++ questions/app.js
@@ -1,11 +1,33 @@
+
 //using selectors inside the element
+const questions = document.querySelectorAll(".question");
 
-//traversing the DOM
+questions.forEach(function (question) {
+    //console.log(orange);
 
-const btns = document.querySelectorAll(".question-btn");
+    const btn = question.querySelector(".question-btn");
+    // console.log(btn);
+    btn.addEventListener("click", function () {
+        //second loop to get the current article
+        questions.forEach(function (item) {
+            if (item !== question) {
+                item.classList.remove("show-text"); //if the article is not the current article
+            }
+        });
 
-btns.forEach(function (btn) {
-    btn.addEventListener("click", function (e) {
-        console.log(e.currentTarget); 
-    })
-})
+        question.classList.toggle("show-text");
+    });
+
+});
+
+
+// //traversing the DOM method
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//     btn.addEventListener("click", function (e) {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//         console.log(question);
+//     })
+// })
