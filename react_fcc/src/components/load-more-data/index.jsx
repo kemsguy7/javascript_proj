162981729eab3636b75db480f205a0a70acd4cb0
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-17 00:25:33 +0100

    finished load more projects

--- react_fcc/src/components/load-more-data/index.jsx
+++ react_fcc/src/components/load-more-data/index.jsx
@@ -1,12 +1,15 @@
 import { useEffect } from 'react'
 import { useState } from 'react'
+import './styles.css'
 
 export default function LoadMoreData() {
   const [loading, setLoading] = useState(false)
   const [products, setProducts] = useState([])
   const [count, setCount] = useState(0)
+  const [disabledButton, setDisabledButton] = useState(false)
 
   async function fetchProducts() {
+    //this function fetches the product from an external API
     try {
       setLoading(true)
       const response = await fetch(
@@ -20,7 +23,8 @@ export default function LoadMoreData() {
       console.log(result)
 
       if (result && result.products && result.products.length) {
-        setProducts(result.products)
+        // setProducts(result.products)
+        setProducts((prevData) => [...prevData, ...result.products])
         setLoading(false)
       }
     } catch (e) {
@@ -31,14 +35,18 @@ export default function LoadMoreData() {
 
   useEffect(() => {
     fetchProducts()
-  }, [])
+  }, [count])
+
+  useEffect(() => {
+    if (products && products.length === 100) setDisabledButton(true) //if the products on a page are up to 100, disable he load more button
+  }, [products])
 
   if (loading) {
     return <div>Loading data ! Please Wait.</div>
   }
 
   return (
-    <div className="container">
+    <div className="load-more-container">
       <div className="product-container">
         {products && products.length
           ? products.map((item) => (
@@ -51,7 +59,10 @@ export default function LoadMoreData() {
       </div>
       <div>
         <div className="button-container">
-          <button>Load More Products</button>
+          <button disabled={disabledButton} onClick={() => setCount(count + 1)}>
+            Load More Products
+          </button>
+          {disabledButton ? <p>You have reached up to 100 products </p> : null}
         </div>
       </div>
     </div>
